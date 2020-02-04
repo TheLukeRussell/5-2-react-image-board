8d557935c42832a165ece215fb377d3b9c76e3ac
Luke Russell <thelukerussel21@gmail.com>
2020-02-04 12:15:38 -0500

    added boilerplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, {Component} from 'react';
-
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class ImageForm extends Component {
+  render() {
+    return(
+      <form>
+      </form>
+    )
+  }
+};
 
-    </div>
-  );
+class ImageList extends Component {
+  render() {
+    return(
+      <ul>
+      </ul>
+    )
+  }
+};
+
+class ImageBoard extends Component {
+  render() {
+    return(
+      <div className='main-app'>
+      <h1>Welcome to My Image Board!</h1>
+      <ImageForm className='image-form' />
+      <ImageList className='image-list' />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default ImageBoard;
